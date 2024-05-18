55481f3b017ca89f9826f231fce96164398350c7
Will Demarest <will.demarest@gmail.com>
2024-05-18 17:30:23 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+In the command prompt, navigate to this directory and enter:
+node index.js
+
+
+Here's the tutorial I got all this from:
+https://socket.io/docs/v4/tutorial/step-4
+
+
+
+I hope you're doing well :)
--- index.js
+++ index.js
@@ -1,11 +1,39 @@
+//cd Code/"Eye Sho"
+//nodemon
+const path = require('path');
 const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
+const fs = require('fs');
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+
+
+
+app.use(express.static('public'));
+
+let roomNum;
+let sim;
+let data = {};
+let gameState;
+
+function Main() {
+  sim = new Sim();
+  InitData();
+  
+
+  roomNum = 0;
+  gameState = new GameState();
+}
+
+function InitData(){
+  data.board = JSON.parse(fs.readFileSync("defaultBoard.json"));
+  data.pieces = JSON.parse(fs.readFileSync("pieces.json"));
+}
+
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
@@ -13,17 +41,39 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('user connected');
+  
+  socket.join(roomNum+'');
+  sendGameState(roomNum);
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
-});
 
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+
+  socket.on('move', (moveMade) => {
+    sendGameState (roomNum);
   });
 });
+
+function sendGameState (roomNum) {
+  io.to(roomNum+'').emit('gameState', gameState);
+}
 
 server.listen(25565, () => {
   console.log('server running at http://localhost:25565');
 });
+
+
+class Sim {
+
+}
+
+class GameState {
+  constructor() {
+    this.initialPosition = [];
+    this.board = data.board;
+
+  }
+}
+
+Main();
